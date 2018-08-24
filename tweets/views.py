bad827071a734e68cfd81b5787a8c160d16b2305
Kiran James <kiranjamespulikkottil@gmail.com>
2018-08-24 23:59:33 +0530

    first commit

--- tweets/views.py
+++ tweets/views.py
@@ -17,7 +17,7 @@ def twitter(request):
     oauth = OAuth1(consumer_key, consumer_sceret, access_token, token_secret)
 
 
-    form = userForm(request.POST)
+    form = userForm(request.GET)
     if form.is_valid():
         username = form.cleaned_data['username']
         url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+username+"&count=10&tweet_mode=extended"
@@ -28,6 +28,7 @@ def twitter(request):
             for tweet in data:
                 tweets.append(tweet['full_text'])
         else:
-            tweets.append('Invalid User Names !..')
-        return render(request, "home.html", {"form" : form, "tweets" : tweets, "username" : username, "code" : response.status_code == 200})
+            tweets.append('Invalid User Name !..')
+
+        return HttpResponse(json.dumps({"tweets" : tweets}))
     return render(request, "home.html", {"form" : form})
